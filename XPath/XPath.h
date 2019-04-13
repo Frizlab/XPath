0a411ee5d36c8e4dff0bfce47705d2ee65338169
Frizlab <fload@me.com>
2019-04-13 10:58:11 +0200

    Initial Commit

---
+++ XPath/XPath.h
@@ -0,0 +1,19 @@
+//
+//  XPath.h
+//  XPath
+//
+//  Created by François Lamboley on 2019/4/13.
+//  Copyright © 2019 Frizlab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XPath.
+FOUNDATION_EXPORT double XPathVersionNumber;
+
+//! Project version string for XPath.
+FOUNDATION_EXPORT const unsigned char XPathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XPath/PublicHeader.h>
+
+
